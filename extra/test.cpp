2655635e0b71f3a30b40905018902861c238ca39
ducvo <pianote@gmail.com>
2021-10-05 16:58:10 -0700

    testing pointer

--- extra/test.cpp
+++ extra/test.cpp
@@ -6,7 +6,7 @@ int *multiply(int a, int b, int *c)
     return c;
 }
 
-void print(int *e)
+void print(string *e)
 {
     cout << "VALUE: = " << *e << endl;
     cout << "ADDRESS: = " << e << endl;
@@ -15,9 +15,18 @@ int main()
 {
     int *ptr = new int;
     *ptr = 8;
-    print (ptr);
+    // print (ptr);
     delete ptr;
     *ptr = 9; 
-    print(ptr);
+    // print(ptr);
     // cout << value << endl;
+    const int SIZE = 3;
+    string* members = new string[SIZE];
+    int numbers[SIZE][SIZE] = {{1,2},{3,4,5},{6,7,8}}; 
+    
+    print(members);
+    print(&members[0]);
+    print(&members[1]);
+    print(&members[2]);
+
 }
