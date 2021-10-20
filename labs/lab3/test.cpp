869e99b60e57a95ab6011eff620f826f45a019b0
Duc Vo <pianote@gmail.com>
2021-10-20 14:45:38 -0700

    small test script

--- labs/lab3/test.cpp
+++ labs/lab3/test.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
 using namespace std;
 bool isPalindrome(string str) {
-    int strLen = str.length();
-    int lastIdx = strLen - 1;
-    char firstChar = str[0];
-    char lastChar = str[lastIdx];
-    string substr;
-
-    if (strLen == 0) return true;
-    if (firstChar != lastChar) return false;
-    substr = str.substr(1, lastIdx - 1);
-    return isPalindrome(substr);
+    if (str.length() == 0) return true;
+    if (str[0] != str[str.length() - 1]) return false;
+    return isPalindrome(str.substr(1, str.length() - 2));
 }
 
 int main() {
-    string text;
-    bool result;
-    text = "tacocat";
-    result = isPalindrome(text);
-    cout << text << (result?" is":" is NOT")
+    string text = "taat";
+    cout << text << (isPalindrome(text)?" is":" is NOT")
          << " a palindrome." << endl;
     return 0;
 }
