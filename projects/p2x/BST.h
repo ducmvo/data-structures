7967a185406af1b28598a3db9233c5cd5ec2d4e0
ducvo <pianote@gmail.com>
2021-11-06 12:09:37 -0700

    create p2x

---
+++ projects/p2x/BST.h
@@ -0,0 +1,307 @@
+//
+// Created by Duc Vo on 10/23/21.
+//
+
+#ifndef P2X_BST_H
+#define P2X_BST_H
+#include <sstream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+class BST {
+public:
+    BST();                                    // Constructor
+    ~BST();                                   // Destructor
+    BST(BST &);                               // Copy Constructor
+    const BST<T> &operator=(BST<T> &);        // Overload = constructor
+    void insert(T val);                       // insert a Node
+    bool contains(T val);                     // determines if node in tree
+    bool remove(T val);                       // remove a Node
+    bool empty();                             // determines if BST is empty
+    int size();                               // count of nodes in BST
+    int getLeafCount();                       // count of nodes without children
+    int getHeight();                          // height of BST. 0 if BST empty
+    int getWidth();                           // width of BST. 0 if BST empty
+    int getLevel(T val);                      // level of a node in BST
+    string getLevelOrder()
+    string getAncestors(T val);               // string of ancestors
+    string getInOrderTraversal();             // string of in-order elements
+    string getPreOrderTraversal();            // string of pre-order elements
+    string getPostOrderTraversal();           // string of post-order elements
+private:
+    struct Node {
+        T data;
+        Node* left;
+        Node* right;
+    };
+    Node* root;                                // Root node of BST
+    int treeSize;                              // hold size of the tree
+    void destroy(Node* &);                     // delete tree
+    void insert(T, Node* &);                   // insert new node to a tree
+    void copy(Node* &, Node* &);               // copy a tree
+    bool contains(T, Node* &);                 // check if tree contain a node
+    bool remove(T val, Node* &);               // remove a Node
+    int size(Node* &, int &);                  // get total node of the tree
+    int getLeafCount(Node* &, int &);          // get total node of the tree
+    int getHeight(Node* &);                    // get height the tree
+    int getLevel(T val, Node* &);
+    void getAncestors(T val, stringstream &ss, Node* &node);
+    void getOrderTraversal(Node* &, stringstream &, string); //ordered traversal
+};
+
+
+// CONSTRUCTOR METHODS
+template <typename T>
+BST<T>::BST() {
+    root = nullptr;
+    treeSize = 0;
+}
+
+template <typename T>
+BST<T>::~BST() {
+    destroy(root);
+}
+
+template <typename T>
+BST<T>::BST(BST &obj) {
+   copy(root, obj.root);
+   treeSize = obj.treeSize;
+}
+
+template <typename T>
+const BST<T> & BST<T>::operator=(BST &obj) {
+    if (this != &obj) {
+        if (root != nullptr) destroy(root);
+        copy(root, obj.root);
+        treeSize = obj.treeSize;
+    }
+    return *this;
+}
+
+//PUBLIC METHODS
+template <typename T>
+void BST<T>::insert(T val) {
+    insert(val, root);
+}
+
+template <typename T>
+bool BST<T>::contains(T val) {
+    if (empty()) return false;
+    return contains(val, root);
+}
+
+template <typename T>
+bool BST<T>::remove(T val) {
+    return remove(val, root);
+}
+
+template <typename T>
+bool BST<T>::empty() {
+    return root == nullptr;
+}
+
+template <typename T>
+int BST<T>::size() {
+    int count = 0;
+    return size(root, count);
+//  return treeSize; // note: treeSize can be used instead
+}
+
+template <typename T>
+int BST<T>::getLeafCount() {
+    int count = 0;
+    return getLeafCount(root, count);
+}
+
+template <typename T>
+int BST<T>::getHeight() {
+    return getHeight(root);
+}
+
+template <typename T>
+int BST<T>::getWidth() {
+    queue<Node> q;
+    
+}
+
+template <typename T>
+int BST<T>::getLevel(T val) {
+    return getLevel(val, root);
+}
+
+template <typename T>
+string BST<T>::getAncestors(T val) {
+    stringstream ss;
+    getAncestors(val, ss, root);
+    return ss.str();
+}
+
+template <typename T>
+string BST<T>::getPreOrderTraversal() {
+    stringstream ss;
+    getOrderTraversal(root, ss, "pre");
+    return ss.str();
+}
+
+template <typename T>
+string BST<T>::getInOrderTraversal() {
+    stringstream ss;
+    getOrderTraversal(root, ss, "in");
+    return ss.str();
+}
+
+template <typename T>
+string BST<T>::getPostOrderTraversal() {
+    stringstream ss;
+    getOrderTraversal(root, ss, "post");
+    return ss.str();
+}
+
+// PRIVATE METHODS
+template <typename T>
+void BST<T>::destroy(Node* &node) {
+    if (node == nullptr) return;
+    destroy(node->left);
+    destroy(node->right);
+    delete node;
+    node = nullptr;
+    treeSize--;
+}
+
+template <typename T>
+void BST<T>::insert(T val, Node* &node) {
+    if (node == nullptr) {
+        Node* newNode = new Node;
+        newNode->data = val;
+        newNode->left = newNode->right= nullptr;
+        node = newNode;
+        treeSize++;
+    } else if (val != node->data)
+        insert(val, val > node->data ? node->right: node->left);
+}
+
+template <typename T>
+void BST<T>::copy(Node* &targetNode, Node* &originalNode) {
+    if (originalNode == nullptr) targetNode = nullptr;
+    else {
+        targetNode = new Node;
+        targetNode->data = originalNode->data;
+        copy(targetNode->left, originalNode->left);
+        copy(targetNode->right, originalNode->right);
+    }
+}
+
+template <typename T>
+bool BST<T>::contains(T val, Node* &node) {
+    if (node == nullptr) return false;
+    if (node->data == val) return true;
+    return contains(val, val>node->data?node->right:node->left);
+}
+
+template <typename T>
+bool BST<T>::remove(T val, Node* &node) {
+    if (node == nullptr) return false;
+
+    if (val != node->data)
+        return remove(val, val > node->data ? node->right : node->left);
+    else {
+        Node* curr = nullptr;
+        // Case 1: No Child
+        // Case 2: One child
+        if(node->left == nullptr) {
+          curr = node;
+          node = node->right;
+          delete curr;
+        }
+        else if (node->right == nullptr) {
+           curr = node;
+           node = node->left;
+           delete curr;
+        }
+        // Case 3: Two children,
+        // replace current node with minimum node from right subtree,
+        // or maximum node from left subtree
+        else {
+            // Method 1: find min of right subtree
+            curr = node->right; // jump to right subtree
+            while (curr->left != nullptr) // traverse to left 
+                curr = curr->left;        // to find min val Node
+            node->data = curr->data; // assign the delete node to found min val
+            return remove(curr->data, node->right); // remove the min val node
+                                                    // from right subtree
+
+            // Method 2: find max of left subtree
+            /*
+            curr = node->left; // jump to left subtree
+            while (curr->right != nullptr) // traverse to right to find max
+                curr = curr->right;
+            node->data = curr->data; // replace node val with max value
+            // recursively remove the node with max value from left subtree
+            remove(curr->data, node->left);
+            */
+        }
+    }
+    treeSize--;
+    return true;
+
+
+
+}
+
+template <typename T>
+int BST<T>::size(Node* &node, int &count) {
+    if (node != nullptr) {
+        count++;
+        size(node->left, count);
+        size(node->right, count);
+    }
+    return count;
+}
+
+template <typename T>
+int BST<T>::getLeafCount(Node* &node, int &count) {
+    if (node != nullptr) {
+        getLeafCount(node->left, count);
+        getLeafCount(node->right, count);
+        if (node->left == nullptr && node->right == nullptr)
+            count++;
+    }
+    return count;
+}
+
+template <typename T>
+int BST<T>::getHeight(Node* &node) {
+    if (node == nullptr)
+        return 0;
+    else
+        return max(getHeight(node->left), getHeight(node->right)) + 1;
+}
+
+template <typename T>
+int BST<T>::getLevel(T val, Node* &node) {
+    if (node->data == val) return 0;
+    else return 1 + getLevel(val, val > node->data ? node->right : node->left);
+
+}
+
+template <typename T>
+void BST<T>::getAncestors(T val, stringstream &ss, Node* &node) {
+    if (node->data != val) {
+        ss << node->data << " ";
+        getAncestors(val, ss, val > node->data ? node->right : node->left);
+    }
+}
+
+template <typename T>
+void BST<T>::getOrderTraversal(Node* &node, stringstream &ss, string type) {
+    if (node != nullptr) {
+        if (type == "pre") ss << node->data << " ";
+        getOrderTraversal(node->left, ss, type);
+        if (type == "in") ss << node->data << " ";
+        getOrderTraversal(node->right, ss, type);
+        if (type == "post") ss << node->data << " ";
+    }
+}
+
+#endif //P2X_BST_H
---
+++ projects/p2x/p2x.cpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <fstream>
+#include "BST.h"
+using namespace std;
+
+template <typename T>
+void displayTreeStats(BST<T> &);
+template <typename T>
+void displayOrderTraversal(BST<T> &);
+template <typename T>
+void displayLevelAndAncestors(BST<T> &, T);
+
+int main() {
+    ifstream inputFile;
+    string fileName, str;
+    int input;
+    string sInput;
+    int testArr[] = {20, 40, 10, 70, 99, -2, 59, 43};
+    string testStringArr[] = {"gene", "mary", "bea", "uma", "yan", "amy", "ron", "opal"};
+
+
+    cout << "Welcome to Binary Search Tree" << endl << endl;
+    cout << "******************************" << endl;
+    cout << "* INTEGER BINARY SEARCH TREE *" << endl;
+    cout << "******************************" << endl ;
+
+    cout << endl << "*********************\n"
+                    "* TEST INTEGERS BST *\n"
+                    "*********************\n" << endl;
+    cout << "** CREATE BST **" << endl;
+    BST<int> intBST;
+
+    displayTreeStats(intBST);
+
+    fileName = "integers.dat";
+    inputFile.open(fileName);
+    if(!inputFile.is_open()) {
+        cout << "\nEnter integer file: ";
+        getline(cin, fileName);
+        inputFile.open(fileName);
+        if(!inputFile.is_open()) {
+            cerr << "Could not open the file - " << fileName << endl;
+            return EXIT_FAILURE;
+        }
+    }
+    cout << endl;
+
+    cout << "** TEST INSERT **" << endl;
+
+    cout << "Inserting in this order: ";
+    while (inputFile >> input) {
+        cout << input << " ";
+        intBST.insert(input);
+    }
+    cout << endl;
+    displayTreeStats(intBST);
+    cout << endl;
+
+    cout << "** TEST TRAVERSALS **" << endl;
+    displayOrderTraversal(intBST);
+    cout << endl;
+
+    cout << "** TEST LEVEL & ANCESTORS **" << endl;
+    str = intBST.getPreOrderTraversal();
+    stringstream ss(str);
+
+    while(ss >> input)
+        displayLevelAndAncestors(intBST, input);
+
+    cout << endl;
+
+    cout << "** TEST CONTAINS **" << endl;
+    for (int val : testArr) {
+        cout << "contains(" << val <<"): "
+             << (intBST.contains(val)?"true":"false") << endl;
+    }
+    cout << endl;
+
+    cout << "** TEST REMOVE **" << endl;
+    cout << "Removing in this order: ";
+    for (int val : testArr) {
+        cout << val << " ";
+        intBST.remove(val);
+    }
+    cout << endl;
+    displayTreeStats(intBST);
+    displayOrderTraversal(intBST);
+    cout << endl;
+
+    cout << "** TEST INSERT (again) **" << endl;
+    cout << "Inserting in this order: ";
+    for (int val : testArr) {
+        cout << val << " ";
+        intBST.insert(val);
+    }
+    cout << endl;
+    displayTreeStats(intBST);
+    displayOrderTraversal(intBST);
+
+    inputFile.close();
+    cout << "\n==================================================" << endl;
+
+    cout << endl << "********************\n"
+                    "* test string BST  *\n"
+                    "********************\n";
+
+    cout << "** CREATE BST **" << endl;
+    BST<string> stringBST;
+
+    displayTreeStats(stringBST);
+
+    fileName = "strings.dat";
+    inputFile.open(fileName);
+    if(!inputFile.is_open()) {
+        cout << "\nEnter strings file: ";
+        getline(cin, fileName);
+        inputFile.open(fileName);
+        if(!inputFile.is_open()) {
+            cerr << "Could not open the file - " << fileName << endl;
+            return EXIT_FAILURE;
+        }
+    }
+    cout << endl;
+
+    cout << "** TEST INSERT **" << endl;
+
+    cout << "Inserting in this order: ";
+    while (inputFile >> sInput) {
+        cout << sInput << " ";
+        stringBST.insert(sInput);
+    }
+    cout << endl;
+    displayTreeStats(stringBST);
+    cout << endl;
+
+    cout << "** TEST TRAVERSALS **" << endl;
+    displayOrderTraversal(stringBST);
+    cout << endl;
+
+    cout << "** TEST LEVEL & ANCESTORS **" << endl;
+    str = stringBST.getPreOrderTraversal();
+    ss.clear();
+    ss.str(str);
+    while(ss >> str) {
+        displayLevelAndAncestors(stringBST, str);
+    }
+    cout << endl;
+
+    cout << "** TEST CONTAINS **" << endl;
+    for (string val : testStringArr) {
+        cout << "contains(" << val <<"): "
+             << (stringBST.contains(val)?"true":"false") << endl;
+    }
+    cout << endl;
+
+    cout << "** TEST REMOVE **" << endl;
+    cout << "Removing in this order: ";
+    for (string val : testStringArr) {
+        cout << val << " ";
+        stringBST.remove(val);
+    }
+    cout << endl;
+
+    displayTreeStats(stringBST);
+    displayOrderTraversal(stringBST);
+    cout << endl;
+
+    cout << "** TEST INSERT (again) **" << endl;
+    cout << "Inserting in this order: ";
+    for (string val : testStringArr) {
+        cout << val << " ";
+        stringBST.insert(val);
+    }
+    cout << endl;
+    displayTreeStats(stringBST);
+    displayOrderTraversal(stringBST);
+
+    cout << endl << "TEST CONTRUCTOR COPY METHODS" << endl;
+    BST<string> st(stringBST);
+    cout<< "COPIED STRING TREE STATS: " << endl;
+    displayTreeStats(st) ;
+    BST<int> st2(intBST);
+    cout<< "COPIED INTEGER TREE STATS: " << endl;
+    displayTreeStats(st2) ;
+
+    cout << "TEST OVERLOAD = OPERATOR" << endl;
+    BST<int> bst;
+    bst = intBST;
+    cout<< "= OPERATOR INT TREE STATS: " << endl;
+    displayTreeStats(bst) ;
+    BST<string> bst2;
+    bst2 = stringBST;
+    cout<< "= OPERATOR STRING TREE STATS: " << endl;
+    displayTreeStats(bst2) ;
+
+    cout << endl << "GoodBye!" << endl;
+
+    inputFile.close();
+    return 0;
+}
+
+template <typename T>
+void displayTreeStats(BST<T> &obj) {
+    cout <<
+        "# of nodes:  " << obj.size() << endl <<
+        "# of leaves: " << obj.getLeafCount() << endl <<
+        "BST height:  " << obj.getHeight() << endl <<
+        "BST empty? " << (obj.empty() ? "true" : "false") << endl;
+}
+
+template <typename T>
+void displayOrderTraversal(BST<T> &obj) {
+    cout <<
+    "pre-order:  " << obj.getPreOrderTraversal() << endl <<
+    "in-order:   " << obj.getInOrderTraversal() << endl <<
+    "post-order: " << obj.getPostOrderTraversal() << endl;
+}
+
+template <typename T>
+void displayLevelAndAncestors(BST<T> &obj, T val) {
+    cout <<
+    "level(" << val << "): " << obj.getLevel(val) << ", "
+    "ancestors(" << val << "): " << obj.getAncestors(val) << endl;
+}
